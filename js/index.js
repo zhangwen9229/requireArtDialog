0a918e9f1d374270d31f9ed4973e196cba7856d9
zhangwen9229 <378253487@qq.com>
2015-06-22 09:53:36 +0800

    添加BUI 库，结合avalon使用

--- js/index.js
+++ js/index.js
@@ -2,24 +2,31 @@ require.config({
     baseUrl: "js",
     paths: {
         jquery: '../node_modules/jquery/dist/jquery', //jQuery目录是放置各种jQuery的版本及其插件的
-        artdialog: '../node_modules/artdialog/dist/dialog'
+        artdialog: '../node_modules/artdialog/dist/dialog',
+        BUI: '../common/buijs/bui'
     },
     shim: {
         "artdialog": {
             deps: ["css!../node_modules/artdialog/css/ui-dialog", "jquery"],
+            exports: "artDialog"
+        },
+        "BUI": {
+            deps: ["css!../common/buijs/css/bs3/bui","css!../common/buijs/css/bs3/dpl"],
             exports: "artDialog"
         }
     }
 });
 
 
-require(["jquery", "artdialog"], function ($) {
+require(["jquery", "artdialog", "BUI"], function ($) {
     console.log("加载完毕");
     var d = dialog({
         title: '欢迎',
         content: '欢迎使用 artDialog 对话框组件！'
     });
-    d.show();
+    $("#btn1").on("click", function (e) {
+        d.show();
+    });
 
     var d1 = dialog({
         title: '提示',
@@ -32,5 +39,14 @@ require(["jquery", "artdialog"], function ($) {
         cancelValue: '取消',
         cancel: function () { }
     });
-    d1.show();
+    $("#btn2").on("click", function (e) {
+        d1.show();
+    });
+
+    BUI.use('bui/calendar', function (Calendar) {
+        var datepicker = new Calendar.DatePicker({
+            trigger: '.calendar',
+            autoRender: true
+        });
+    });
 });
